e792b8b39a8416987b6f89e746c45ba8a72fe0fc
namefox <thiiagoo912@hotmail.com>
2023-02-19 15:38:26 -0300

    Fix releases and security bug, add links in project pages

--- assets/repo.js
+++ assets/repo.js
@@ -8,6 +8,8 @@ for (let i = 0; i < 3; i++) {
 const urlParams = new URLSearchParams(window.location.search);
 const repoParam = urlParams.get('repo');
 
+githubLink.href = "https://github.com/namefox/" + repoParam;
+
 const findReadme = (json) => {
     return new Promise((resolve) => {
         let found = false;
@@ -57,7 +59,47 @@ const loadJSON = (json) => {
     shortcutIcon.href = findImage(json);
 };
 
+const loadReleases = (json) => {
+    repos.innerHTML = "";
+    json.forEach((item) => {
+        const div = document.createElement("div");
+        div.id = item.name;
+        div.classList.add("repo");
+
+        const h1 = document.createElement("h2");
+
+        h1.innerText = item.name.replaceAll("-", " ") + (item.fork ? " (fork)" : "");
+        div.append(h1);
+
+        const links = document.createElement("p");
+
+        links.innerHTML = `<a href="${item.zipball_url}">zip</a> | <a href="${item.tarball_url}">tar</a>`;
+
+        div.append(links);
+
+        const description = document.createElement("p");
+        description.classList.add("description", "markdown");
+        description.innerHTML = marked.parse(item.body);
+        div.append(description);
+
+        repos.append(div);
+    });
+
+    if (repos.childElementCount == 0) {
+        repos.innerHTML = "<p><br>nothing here yet :(</p>";
+    }
+};
+
 let saved = sessionStorage.getItem(repoParam + "Contents");
+let savedReleases = sessionStorage.getItem(repoParam + "Releases");
+
+if (repoParam.includes("../")) {
+    loadJSON([{name:"README.md",download_url:"./different-user.md"}]);
+    loadReleases([]);
+
+    throw new Error("Security: Can not access repositories from different account");
+}
+
 if (saved) {
     console.log("Project data gathered from session");
     loadJSON(JSON.parse(saved));
@@ -66,5 +108,16 @@ if (saved) {
         loadJSON(json);
         sessionStorage.setItem(repoParam + "Contents", JSON.stringify(json));
         console.log("Project data requested from API");
+    });
+}
+
+if (savedReleases) {
+    loadReleases(JSON.parse(savedReleases));
+    console.log("Project releases gathered from session");
+} else {
+    fetch(`https://api.github.com/repos/namefox/${repoParam}/releases`).then((r) => r.json()).then((json) => {
+        loadReleases(json);
+        sessionStorage.setItem(repoParam + "Releases", JSON.stringify(json));
+        console.log("Project releases gathered from API");
     });
 }
